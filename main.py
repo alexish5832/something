1157f3fc52d492a2d734bc357d916ce50dcc2c29
alexish5832 <144606285+alexish5832@users.noreply.github.com>
2024-03-16 22:13:07 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from model import get_class
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'we have logged in as {bot.user}')
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment. filename
+            file_url = attachment.url
+            await attachment.save(f"./{attachment.filename}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="labels.txt", image_path=f"./{attachment.filename}"))
+    else:
+        await ctx.send("Вы забыли загрузить картинку :(")
+
+bot.run("MTE1ODAxOTQ2MTM3MTEzNDA5Mw.GLvifW.b5LcTGA2DSwOIIjWzcI24Ntez6nC50LcCqcTJY")
---
+++ model.py
@@ -0,0 +1,26 @@
+from keras.models import load_model
+from PIL import Image, ImageOps
+import numpy as np
+
+def get_class(model_path, labels_path, image_path):
+    np.set_printoptions(suppress=True)
+    model = load_model(model_path, compile=False)
+    class_names = open(labels_path, "r", encoding="utf-8").readlines()
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = Image.open(image_path).convert("RGB")
+
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+    image_array = np.asarray(image)
+
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return(class_name[2:], confidence_score)
